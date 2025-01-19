0b955270ca8f0be1cf99e80c9a1481f64f891c35
dnzhou <zhou.danny@hotmail.com>
2025-01-19 17:16:25 +1100

    fixed file names to solve naming conflicts in rojo

--- src/ServerScriptService/Initialise.server.lua
+++ src/ServerScriptService/Initialise.server.lua
@@ -1,4 +1,4 @@
-local Players = game:GetService("Players")
+-- local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
 
 -- Step 1: Create a Folder in Workspace to store player models
